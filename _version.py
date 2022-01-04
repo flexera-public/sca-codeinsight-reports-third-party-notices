b9c07a6373334689e56e708bb6ffaaba09645449
Sean Geary <sgeary@flexera.com>
2022-01-04 14:04:12 -0500

    Add version file

---
+++ _version.py
@@ -0,0 +1,10 @@
+'''
+Copyright 2021 Flexera Software LLC
+See LICENSE.TXT for full license text
+SPDX-License-Identifier: MIT
+
+Author : sgeary  
+Created On : Wed Dec 15 2021
+File : _version.py
+'''
+__version__ = "1.0.1"
